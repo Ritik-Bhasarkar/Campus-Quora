c61597b0c3e2cb54d72e59fff6f3c273984eb053
Ritik Bhasarkar <ritikbhasarkar2401@gmail.com>
2023-10-04 04:21:34 +0530

    video got worse

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -1,32 +1,29 @@
-import {useEffect,useState,useRef} from 'react';
+import {useEffect,useState} from 'react';
 import {FcGoogle} from 'react-icons/fc'
 import {BsGithub} from 'react-icons/bs'
+import videoBg from '../resource/videos/background-vid.mp4'
 
 const Login = () => {
-    const headerRef = useRef(null);
-    const [isGradientComplete, setIsGradientComplete] = useState(false);
-    useEffect(()=>{
-       const updateMousePosition =(MouseEvent)=>{
-            if(!headerRef.current) return;
-            const {clientX} = MouseEvent;
-            headerRef.current.style.setProperty('--x',`${clientX}px`);
-            
-       } 
-       window.addEventListener('mousemove',updateMousePosition);
-       return()=>{
-        window.removeEventListener('mousemove',updateMousePosition)
-       }
-    },[])
+   
 
 
 
   return (
-    <div className='login-container' ref={headerRef}>
+    <div className='login-container'>
+       
+        {/* <div className='website-name'>
+            <h2>Campus-Quora</h2>
+        </div> */}
         <div className='login-container__card'>
-        <div className={`login-header ${isGradientComplete ? 'text-black' : ''}`} ref={headerRef}>
+            <div className='bg-video'>
+                <video autoplay loop muted>
+                <source src={videoBg} type='video/mp4'/>
+                </video>
+            </div>
+         <div className='login-component'>
+            <div className='login-header'>
             <h1>Login</h1>
-        </div>
-        <div className='login-component'>
+            </div>
             <div className='login-socials'>
               <div className='login__byGoogle'>
                 <button className='login-btn'>
@@ -54,7 +51,6 @@ const Login = () => {
                 <button className='login-form btn'>Sign In</button>
             </form>
         </div>
-
         </div>
     </div>
   )
--- src/setupTests.js
+++ src/setupTests.js
@@ -3,3 +3,19 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
+
+
+ // const headerRef = useRef(null);
+    // //const [isGradientComplete, setIsGradientComplete] = useState(false);
+    // useEffect(()=>{
+    //    const updateMousePosition =(MouseEvent)=>{
+    //         if(!headerRef.current) return;
+    //         const {clientX} = MouseEvent;
+    //         headerRef.current.style.setProperty('--x',`${clientX}px`);
+            
+    //    } 
+    //    window.addEventListener('mousemove',updateMousePosition);
+    //    return()=>{
+    //     window.removeEventListener('mousemove',updateMousePosition)
+    //    }
+    // },[])
